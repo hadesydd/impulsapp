694dea6b4b13376b838edf5028cf7597aca518cb
Temmamra Amine <125040682+hadesydd@users.noreply.github.com>
2023-12-23 16:11:40 +0000

    updated

--- main.tf
+++ main.tf
@@ -2,53 +2,106 @@ provider "azurerm" {
   features {}
 }
 
-# refer to a resource group
-data "azurerm_resource_group" "test" {
-  name = "atlas-pool_group"
+# Create a new resource group
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "my-rg"
+  location = "eastus"
 }
 
-#refer to a subnet
-data "azurerm_subnet" "test" {
-  name                 = "default"
-  virtual_network_name = "tutu"
-  resource_group_name  = "atlas-pool_group"
-}
+# Create a new NSG that allows SSH, HTTP, and HTTPS
+resource "azurerm_network_security_group" "my_nsg" {
+  name                = "my-nsg"
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
 
-# Create public IPs
-resource "azurerm_public_ip" "test" {
-    name                = "myPublicIP"
-    location            = data.azurerm_resource_group.test.location
-    resource_group_name = data.azurerm_resource_group.test.name
-    allocation_method   = "Dynamic"
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-}
+  security_rule {
+    name                       = "allow_http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-# create a network interface
-resource "azurerm_network_interface" "test" {
-  name                = "nic-test"
-  location            = "${data.azurerm_resource_group.test.location}"
-  resource_group_name = "${data.azurerm_resource_group.test.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${data.azurerm_subnet.test.id}"
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.2.0.10" 
-    public_ip_address_id          = "${azurerm_public_ip.test.id}"
+  security_rule {
+    name                       = "allow_https"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "myvm"
-  location              = data.azurerm_resource_group.test.location
-  resource_group_name   = data.azurerm_resource_group.test.name
-  network_interface_ids = [azurerm_network_interface.test.id]
+# Create a new VNet with a subnet
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+}
+
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.my_resource_group.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_public_ip" {
+    name                = "myPublicIP"
+    location            = azurerm_resource_group.my_resource_group.location
+    resource_group_name = azurerm_resource_group.my_resource_group.name
+    allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "my_nic" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name 
+  
+  ip_configuration {
+    name                          = "my-configuration"
+    subnet_id                     = azurerm_subnet.my_subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.10" 
+    public_ip_address_id          = azurerm_public_ip.my_public_ip.id
+  }
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_nic.id
+  network_security_group_id = azurerm_network_security_group.my_nsg.id
+}
+
+# Create a new virtual machine in the new resource group and VNet
+resource "azurerm_virtual_machine" "my_vm" {
+  name                  = "my-vm"
+  location              = azurerm_resource_group.my_resource_group.location
+  resource_group_name   = azurerm_resource_group.my_resource_group.name
+  network_interface_ids = [azurerm_network_interface.my_nic.id]
   vm_size               = "Standard_DS1_v2"
 
-  
-
   storage_os_disk {
-    name              = "myosdisk"
+    name              = "myosdisk2"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -62,18 +115,14 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = "myvm"
+    computer_name  = "my-vm"
     admin_username = "superuser"
     admin_password = "Admin-456"
     custom_data = filebase64("custom_data.sh")
-
   }
-
-
 
   os_profile_linux_config {
     disable_password_authentication = false
-
   }
 
   tags = {
