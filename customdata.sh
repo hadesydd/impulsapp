ab3b510bbe122487c4380e0e88d5bca14a81d783
Temmamra Amine <125040682+hadesydd@users.noreply.github.com>
2023-12-23 12:38:06 +0000

    added customdata

---
+++ customdata.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install python3
+sudo apt install ansible -y
+sudo apt install ansible-core -y
+ansible --version
+
+
+git clone https://github.com/hadesydd/impulsapp.git
+export IMPULSAPP_PATH=$(pwd)/impulsapp/playbook
+source ~/.bashrc
+cd $IMPULSAPP_PATH
+chmod +x *.sh
+
+
+
+ansible-playbook -i localhost test.yml
+ansible-playbook -i localhost create.yml
+ansible-playbook -i localhost apache.yml
