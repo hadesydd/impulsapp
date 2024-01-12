a054afd44d718ca033cd10cba636a27c6c0b4e39
Temmamra Amine <125040682+hadesydd@users.noreply.github.com>
2024-01-12 00:36:56 +0000

    nodeINSTALL

--- custom_data.sh
+++ custom_data.sh
@@ -18,7 +18,12 @@ vhost_path="impulsapp/vhost"
 export full_path="$user_home/$vhost_path"
 source ~/.bashrc
 ansible-playbook -i localhost php.yml
-ansible-playbook -i localhost nodejs-12.yml
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm  
+nvm install 12
+nvm use 12
 ansible-playbook -i localhost symfony.yml
 #sudo sed -n '60p' /var/lib/cloud/instance/user-data.txt >> cd $IMPULSAPP_PATH/vars.yml
 
