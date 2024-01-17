c48ce2dd3a234d09eb5ca7b1000eadc147eac3dd
Temmamra Amine <125040682+hadesydd@users.noreply.github.com>
2024-01-17 13:55:19 +0000

    custom change

--- custom_data.sh
+++ custom_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install ansible
 sudo apt-get update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install python3 -y
 sudo apt install ansible -y
